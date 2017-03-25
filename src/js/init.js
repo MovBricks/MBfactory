77c1faff99f599ad6948a7cf5fd26f833964b676
engineer <youngbird92@gmai.com>
2017-03-25 15:05:19 +0800

    用js修复浏览器兼容提示

--- src/js/init.js
+++ src/js/init.js
@@ -12,12 +12,26 @@
         this.box = box;
         this.file = file;
     }
-
+    
+     function isIE() { //ie?
+        var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+        
+        if (!!window.ActiveXObject || "ActiveXObject" in window || userAgent.indexOf("Edge") > -1)
+            return true;
+        else
+            return false;
+    }
+    
 
     //初始化函数订阅移除事件数组
     var startList = [];
     var exitList = [];
-    window.onload = function () {      
+    window.onload = function () {  
+        if(isIE()){
+            var isIETips = document.getElementById("isIETips");
+            isIETips.setAttribute("class", "browserupgrade show"); //设置class属性   
+        }
+
         for(var loop = 0;loop<startList.length;loop++){
             var func = startList[loop]; 
             func();
